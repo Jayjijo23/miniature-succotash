e11a530c2370090c13090b33a256ae90de7e7c3c
Jayjijo23 <162903326+Jayjijo23@users.noreply.github.com>
2024-03-10 12:58:37 -0400

    fix logic

--- script.js
+++ script.js
@@ -2,46 +2,35 @@ let searchButton = document.getElementById('searchButton');
 let recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
 let isListening = false;
 
-searchButton.addEventListener('click', function() {
-    if (!isListening) {
-        startRecording();
-    } else {
-        stopRecording();
-    }
-});
-
-function startRecording() {
-    isListening = true;
-    searchButton.classList.add('recording');
-    searchButton.innerText = 'Record';
-    recognition.start();
-}
-
-function stopRecording() {
-    isListening = false;
-    recognition.stop();
-    searchButton.classList.remove('recording');
-    searchButton.classList.add('searching');
-    searchButton.innerText = 'Searching...';
-}
-
 recognition.onresult = function(event) {
-    let transcript = event.results[0][0].transcript;
-    performSearch(transcript);
+    performSearch(event.results[0][0].transcript);
 };
 
 recognition.onend = function() {
-    if (!isListening) {
+    if (isListening) {
+        isListening = false;
         resetButton();
     }
 };
 
+searchButton.addEventListener('click', function() {
+    if (!isListening) {
+        isListening = true;
+        searchButton.classList.add('recording');
+        searchButton.innerText = 'Record';
+        recognition.start();
+    } else {
+        recognition.stop();
+    }
+});
+
 function performSearch(query) {
+    searchButton.classList.add('searching');
+    searchButton.innerText = 'Searching...';
     window.open(`https://google.com/search?q=${encodeURIComponent(query)}`, '_blank');
-    resetButton();
 }
 
 function resetButton() {
-    searchButton.classList.remove('searching');
+    searchButton.classList.remove('recording', 'searching');
     searchButton.innerText = 'Search';
 }
