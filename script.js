a065f87983fdb3fd153775f46a4b9ba865f8a880
Jayjijo23 <162903326+Jayjijo23@users.noreply.github.com>
2024-03-10 13:29:10 -0400

    make button red when recording

--- script.js
+++ script.js
@@ -2,34 +2,32 @@ let searchButton = document.getElementById('searchButton');
 let recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
 let isListening = false;
 
+recognition.onstart = function() {
+    isListening = true;
+    searchButton.classList.add('recording');
+    searchButton.innerText = 'Record';
+    searchButton.style.backgroundColor = '#f44336'; // Turn the button red
+};
+
+recognition.onend = function() {
+    isListening = false;
+    searchButton.classList.remove('recording');
+    searchButton.style.backgroundColor = ''; // Reset the button color
+    searchButton.innerText = 'Search';
+};
+
 recognition.onresult = function(event) {
     let transcript = event.results[0][0].transcript;
     performSearch(transcript);
 };
 
-recognition.onend = function() {
-    if (isListening) {
-        toggleListening();
-    }
-};
-
 searchButton.addEventListener('click', function() {
     if (!isListening) {
-        toggleListening();
-    }
-});
-
-function toggleListening() {
-    if (!isListening) {
-        isListening = true;
-        searchButton.innerText = 'Record';
         recognition.start();
     } else {
-        isListening = false;
-        searchButton.innerText = 'Search';
-        recognition.stop(); // Stop recording
+        recognition.stop();
     }
-}
+});
 
 function performSearch(query) {
     window.open(`https://google.com/search?q=${encodeURIComponent(query)}`, '_blank');
